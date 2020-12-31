44afabd1743c01c663422aad153dc645eebbf008
lthoangg <letronghoang00@gmail.com>
2020-12-31 09:11:27 +0700

    Update successfully train and test

--- test.py
+++ test.py
@@ -7,7 +7,6 @@ from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.layers import MaxPooling2D
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
-
 emotion_model = Sequential()
 
 emotion_model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(48,48,1)))
@@ -25,19 +24,22 @@ emotion_model.add(Flatten())
 emotion_model.add(Dense(1024, activation='relu'))
 emotion_model.add(Dropout(0.5))
 emotion_model.add(Dense(7, activation='softmax'))
-emotion_model.load_weights('emotion_model.h5')
+emotion_model.load_weights('./model/emotion_model.h5')
 
 cv2.ocl.setUseOpenCL(False)
 
 emotion_dict = {0: "Angry", 1: "Disgusted", 2: "Fearful", 3: "Happy", 4: "Neutral", 5: "Sad", 6: "Surprised"}
 
-cap = cv2.VideoCapture(0)
+
+# start the webcam feed
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 while True:
+    # Find haar cascade to draw bounding box around face
     ret, frame = cap.read()
     if not ret:
         break
-    bounding_box = cv2.CascadeClassifier('haarcascade/haarcascade_frontalface_default.xml')
-    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2gray_frame)
+    bounding_box = cv2.CascadeClassifier('./haarcascade/haarcascade_frontalface_default.xml')
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     num_faces = bounding_box.detectMultiScale(gray_frame,scaleFactor=1.3, minNeighbors=5)
 
     for (x, y, w, h) in num_faces:
@@ -50,6 +52,7 @@ while True:
 
     cv2.imshow('Video', cv2.resize(frame,(1200,860),interpolation = cv2.INTER_CUBIC))
     if cv2.waitKey(1) & 0xFF == ord('q'):
-        cap.release()
-        cv2.destroyAllWindows()
         break
+
+cap.release()
+cv2.destroyAllWindows()
